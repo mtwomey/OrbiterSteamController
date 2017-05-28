cd71aa4affb9e94ce9fab294f8b1853654a03241
Matthew Twomey <mtwomey@beakstar.com>
2017-05-27 21:47:02 -0500

    Reworked Xinput stuff into a class

--- OrbiterSteamController/XinputController.cpp
+++ OrbiterSteamController/XinputController.cpp
@@ -1,12 +1,41 @@
 #include "XinputController.h"
 
 
-
-XinputController::XinputController()
-{
+XinputController::XinputController() {
+	init();
 }
 
 
-XinputController::~XinputController()
+XinputController::~XinputController() {
+
+}
+
+void XinputController::init() {
+	setControllerNumber();
+	updateState();
+}
+
+void XinputController::updateState() {
+	ZeroMemory(&state, sizeof(XINPUT_STATE));
+	XInputGetState(controllerNumber, &state);
+}
+
+int XinputController::getLX()
 {
+	updateState();
+	XInputGetState(controllerNumber, &state);
+	return state.Gamepad.sThumbLX;
+}
+
+void XinputController::setControllerNumber() // Sets the controller to the first connected controller
+{
+	int controllerId = -1;
+	for (DWORD i = 0; i < XUSER_MAX_COUNT && controllerId == -1; i++)
+	{
+		ZeroMemory(&state, sizeof(XINPUT_STATE));
+
+		if (XInputGetState(i, &state) == ERROR_SUCCESS)
+			controllerId = i;
+	}
+	controllerNumber = controllerId;
 }
--- OrbiterSteamController/XinputController.h
+++ OrbiterSteamController/XinputController.h
@@ -1,8 +1,19 @@
 #pragma once
+#include <Windows.h>
+#include <xinput.h>
+
 class XinputController
 {
+	int controllerNumber;
+	XINPUT_STATE state;
 public:
 	XinputController();
 	~XinputController();
+
+	void init();
+	void updateState();
+	int getLX();
+private:
+	void setControllerNumber();
 };
 
