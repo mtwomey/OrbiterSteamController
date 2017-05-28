18b29a94c501342c0e13756bb0914d826a33487f
Matthew Twomey <mtwomey@beakstar.com>
2017-05-28 01:23:58 -0500

    Added a debug flag

--- OrbiterSteamController/main.cpp
+++ OrbiterSteamController/main.cpp
@@ -1,6 +1,8 @@
 #define STRICT
 #define ORBITER_MODULE
 
+//#define DEBUG
+
 #include <Orbitersdk.h>
 #include "XinputController.h"
 
@@ -11,10 +13,15 @@ DLLCLBK void InitModule(HINSTANCE hModule) {
 }
 
 DLLCLBK void opcPreStep(double simt, double simdt, double mjd) {
-	sprintf(oapiDebugString(), "X: %f Y: %f R: %f", controller.getNormLX(), controller.getNormLY(), controller.getNormRT() + (controller.getNormLT() * -1));
+    #ifdef DEBUG
+	sprintf(oapiDebugString(), "LX: %f LY: %f LT+RT: %f RX: %f RY: %f", controller.getNormLX(), controller.getNormLY(), controller.getNormRT() + (controller.getNormLT() * -1), controller.getNormRX(), controller.getNormRY());
+    #endif
 
 	VESSEL* vessel = oapiGetFocusInterface();
 	vessel->SetControlSurfaceLevel(AIRCTRL_ELEVATOR, controller.getNormLY() * -1, false);
 	vessel->SetControlSurfaceLevel(AIRCTRL_AILERON, controller.getNormLX(), false);
 	vessel->SetControlSurfaceLevel(AIRCTRL_RUDDER, controller.getNormRT() + (controller.getNormLT() * -1), false);
+
+	// An experiment with controlling the view with the RS directly. Not really needed as you can bind either pad to a mouse with the right button held down
+	//oapiCameraSetCockpitDir((roundf(controller.getNormRX() * 1000) / 1000) * -1, roundf(controller.getNormRY() * 1000) / 1000, false);
 }
